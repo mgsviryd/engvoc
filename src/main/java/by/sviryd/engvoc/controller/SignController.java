1977eba8b202293caba646815b6cbe752cce1ab9
mgsviryd <mgsviryd@gmail.com>
2023-06-17 18:40:09 +0300

    ui: logo and favicon

--- src/main/java/by/sviryd/engvoc/controller/SignController.java
+++ src/main/java/by/sviryd/engvoc/controller/SignController.java
@@ -2,6 +2,7 @@ package by.sviryd.engvoc.controller;
 
 import by.sviryd.engvoc.domain.User;
 import by.sviryd.engvoc.domain.VerificationToken;
+import by.sviryd.engvoc.service.MessageI18nService;
 import by.sviryd.engvoc.service.SignUpUserService;
 import by.sviryd.engvoc.service.UserService;
 import by.sviryd.engvoc.service.VerificationTokenService;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 import java.util.UUID;
 
 @Controller
@@ -29,14 +32,18 @@ public class SignController {
     private UserService userService;
     @Autowired
     private VerificationTokenService verificationTokenService;
+    @Autowired
+    private MessageI18nService messageI18nService;
 
     @GetMapping("/up")
-    public String up() {
+    public String up(Model model, Locale locale) {
+        model.addAttribute("title", messageI18nService.getMessage("titleSignUp", new Object[]{}, locale));
         return "main";
     }
 
     @GetMapping("/in")
-    public String in() {
+    public String in(Model model, Locale locale) {
+        model.addAttribute("title", messageI18nService.getMessage("titleSignIn", new Object[]{}, locale));
         return "main";
     }
 
